6d585bec0fb0e37441f831773a29073b644a0fc7
ydam-k19 <ydamk15nbk@gmail.com>
2023-05-21 18:33:00 +0700

    clone feature

--- display/editor/RenderNode.tsx
+++ display/editor/RenderNode.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
 import ArrowUp from '../../public/icons/arrow-up.svg';
+import Clone from '../../public/icons/clone.svg';
 import Delete from '../../public/icons/delete.svg';
 import Move from '../../public/icons/move.svg';
 import Link from '@mui/icons-material/TrendingUp';
@@ -24,14 +25,12 @@ import { MOCK_COLLECTIONS, MOCK_DOCUMENTS } from 'display/mock/dynamic-data';
 import { lighten, darken } from '@mui/system';
 import { createFilterOptions } from '@mui/material/Autocomplete';
 
-
 const GroupHeader = styled('div')({
   position: 'sticky',
   top: '-8px',
   padding: '4px 10px',
   color: _var.secondaryColor,
-  backgroundColor:
-    lighten(_var.secondaryColor, 0.85),
+  backgroundColor: lighten(_var.secondaryColor, 0.85),
 });
 
 const GroupItems = styled('ul')({
@@ -43,7 +42,7 @@ const IndicatorDiv = styled.div`
   margin-top: -29px;
   font-size: 14px;
   line-height: 14px;
-  .link{
+  .link {
     font-size: 20px;
     line-height: 20px;
     svg {
@@ -76,7 +75,6 @@ export const RenderNode = ({ render }) => {
     isActive: query.getEvent('selected').contains(id),
     database: query.getDatabase(),
   }));
-
 
   const {
     isHover,
@@ -136,15 +134,13 @@ export const RenderNode = ({ render }) => {
     };
   }, [scroll]);
 
-
-
   const [openDialogConnectData, setOpenDialogConnectData] = useState(false);
   const [collections, setCollections] = useState([]);
   const [documents, setDocuments] = useState([]);
   const [selectedCollection, setSelectedCollection] = useState(null);
   const [selectedDocument, setSelectedDocument] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     setCollections(database.collections);
     setDocuments(database.documents);
   }, [database]);
@@ -156,31 +152,34 @@ export const RenderNode = ({ render }) => {
     handleCloseDialogConnectData();
     setProp((props) => {
       props.text = {
-        type: "dynamic",
+        type: 'dynamic',
         value: selectedDocument.label,
         key: selectedDocument.key,
         collectionId: selectedCollection.value,
         documentId: selectedDocument.value.slice(0, Number(selectedDocument.value.indexOf('-'))),
       };
     });
-  }
+  };
 
   const collectionOptions = collections?.map((item) => {
     return {
       value: item.id,
       label: item.name,
-    }
+    };
   });
 
-  const documentOptions = documents?.filter(item => item.collectionId === selectedCollection?.value).map((item) => {
-    return Object.entries(item.data).map(([key, value]) => {
-      return ({
-        value: `${item.id}-${key}`,
-        label: value,
-        key,
-      })
+  const documentOptions = documents
+    ?.filter((item) => item.collectionId === selectedCollection?.value)
+    .map((item) => {
+      return Object.entries(item.data).map(([key, value]) => {
+        return {
+          value: `${item.id}-${key}`,
+          label: value,
+          key,
+        };
+      });
     })
-  }).flat();
+    .flat();
 
   const filterOptions = createFilterOptions({
     matchFrom: 'start',
@@ -193,96 +192,123 @@ export const RenderNode = ({ render }) => {
     if (!selectedCollection) {
       setSelectedDocument(null);
     }
-  }, [selectedCollection])
+  }, [selectedCollection]);
 
   return (
     <>
       {isHover || isActive || isShownAllIndicator
         ? ReactDOM.createPortal(
-          <IndicatorDiv
-            ref={currentRef}
-            className='px-2 py-2 text-white bg-blue-500 fixed flex items-center'
-            style={{
-              left: getPos(dom).left,
-              top: getPos(dom).top,
-              zIndex: 9999,
-            }}
-          >
-            <h2 className='flex-1'>{name}</h2>
-            <p className='text-sm text-yellow-300 mr-4 ml-2'>{id}</p>
-            {moveable ? (
-              <Btn className='mr-2 cursor-move' ref={drag}>
-                <Move />
-              </Btn>
-            ) : null}
-            {!id.startsWith(ROOT_NODE) && (
-              <Btn
-                className='mr-2 cursor-pointer'
-                onClick={() => {
-                  actions.selectNode(parent);
-                }}
-              >
-                <ArrowUp />
-              </Btn>
-            )}
-            {deletable ? (
-              <Btn
-                className='mr-2 cursor-pointer'
-                onMouseDown={(e: React.MouseEvent) => {
-                  e.stopPropagation();
-                  actions.delete(id);
-                }}
-              >
-                <Delete />
-              </Btn>
-            ) : null}
-            {propTextValue !== undefined ? (
-              <Tooltip title='Connect Data'>
+            <IndicatorDiv
+              ref={currentRef}
+              className='px-2 py-2 text-white bg-blue-500 fixed flex items-center'
+              style={{
+                left: getPos(dom).left,
+                top: getPos(dom).top,
+                zIndex: 9999,
+              }}
+            >
+              <h2 className='flex-1'>{name}</h2>
+              <p className='text-sm text-yellow-300 mr-4 ml-2'>{id}</p>
+              {moveable ? (
+                <Btn className='mr-2 cursor-move' ref={drag}>
+                  <Move />
+                </Btn>
+              ) : null}
+              {!id.startsWith(ROOT_NODE) && (
                 <Btn
-                  className='link cursor-pointer'
-                  onMouseDown={(e: React.MouseEvent) => {
-                    e.stopPropagation();
-                    setOpenDialogConnectData(true);
+                  className='mr-2 cursor-pointer'
+                  onClick={() => {
+                    const {
+                      data: { type, props },
+                    } = query.node(id).get();
+                    actions.add(query.createNode(React.createElement(type, props)), parent);
                   }}
                 >
-                  <Link />
+                  <Clone />
                 </Btn>
-              </Tooltip>
-            ) : null}
-          </IndicatorDiv>,
-          document.querySelector('.page-container') // create inside the page container, but still in component tree (triggered by onClick...)
-        )
+              )}
+              {!id.startsWith(ROOT_NODE) && (
+                <Btn
+                  className='mr-2 cursor-pointer'
+                  onClick={() => {
+                    actions.selectNode(parent);
+                  }}
+                >
+                  <ArrowUp />
+                </Btn>
+              )}
+              {deletable ? (
+                <Btn
+                  className='mr-2 cursor-pointer'
+                  onMouseDown={(e: React.MouseEvent) => {
+                    e.stopPropagation();
+                    actions.delete(id);
+                  }}
+                >
+                  <Delete />
+                </Btn>
+              ) : null}
+              {propTextValue !== undefined ? (
+                <Tooltip title='Connect Data'>
+                  <Btn
+                    className='link cursor-pointer'
+                    onMouseDown={(e: React.MouseEvent) => {
+                      e.stopPropagation();
+                      setOpenDialogConnectData(true);
+                    }}
+                  >
+                    <Link />
+                  </Btn>
+                </Tooltip>
+              ) : null}
+            </IndicatorDiv>,
+            document.querySelector('.page-container') // create inside the page container, but still in component tree (triggered by onClick...)
+          )
         : null}
       {render}
 
       {/* Dialog connect data for text */}
 
-      <Dialog open={openDialogConnectData} onClose={handleCloseDialogConnectData} aria-labelledby='form-dialog-connect-data-title' id="form-dialog-connect-data">
-        <DialogTitle >Connect Data From Your Database</DialogTitle>
+      <Dialog
+        open={openDialogConnectData}
+        onClose={handleCloseDialogConnectData}
+        aria-labelledby='form-dialog-connect-data-title'
+        id='form-dialog-connect-data'
+      >
+        <DialogTitle>Connect Data From Your Database</DialogTitle>
         <DialogContent id='form-dialog-connect-data-content'>
           <DialogContentText>Choose your data's value from your collections</DialogContentText>
 
           <Autocomplete
             disablePortal
-            id="collections-autocomplete"
+            id='collections-autocomplete'
             options={collectionOptions}
             sx={{ width: 300 }}
-            onChange={(_, newValue) => { setSelectedCollection(newValue); }}
+            onChange={(_, newValue) => {
+              setSelectedCollection(newValue);
+            }}
             value={selectedCollection}
-            renderInput={(params) => <TextField {...params} label="Collection"
-              // @ts-ignore
-              filterOptions={filterOptions} />}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label='Collection'
+                // @ts-ignore
+                filterOptions={filterOptions}
+              />
+            )}
           />
 
           <Autocomplete
-            id="documents-autocomplete"
+            id='documents-autocomplete'
             value={selectedDocument}
             options={documentOptions?.sort((a, b) => (a.value < b.value ? -1 : 1))}
             disabled={!selectedCollection}
-            groupBy={(option) => (option.value.slice(0, Number(option.value.indexOf('-'))) as any)}
-            getOptionLabel={(option) => (`(${option.key}) ${option.label.length > 47 ? `${option.label?.slice(0, 47)}...` : option.label}` as any)}
+            groupBy={(option) => option.value.slice(0, Number(option.value.indexOf('-'))) as any}
+            getOptionLabel={(option) =>
+              `(${option.key}) ${option.label.length > 47 ? `${option.label?.slice(0, 47)}...` : option.label}` as any
+            }
             sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Choose data from documents" />}
+            renderInput={(params) => <TextField {...params} label='Choose data from documents' />}
             renderGroup={(params) => (
               <li key={params.key}>
                 <GroupHeader>Document {params.group}</GroupHeader>
@@ -294,7 +320,6 @@ export const RenderNode = ({ render }) => {
             }}
             filterOptions={filterOptions}
           />
-
         </DialogContent>
         <DialogActions>
           <MaterialButton
