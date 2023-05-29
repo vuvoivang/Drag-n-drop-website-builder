91553a2730f9304061803f150f911a53e5545036
Võ Hoàng Vũ <vuprotk21@gmail.com>
2023-05-29 08:13:49 +0700

    fix: root node current page Layer

--- libs/layers/src/index.tsx
+++ libs/layers/src/index.tsx
@@ -4,12 +4,17 @@ import React from 'react';
 import { LayerOptions } from './interfaces';
 import { LayerContextProvider } from './layers/LayerContextProvider';
 import { LayerManagerProvider } from './manager/LayerManagerProvider';
+import { useEditor } from '@libs/hooks';
 export { useLayer } from './layers';
 
 export const Layers: React.FC<Partial<LayerOptions>> = ({ ...options }) => {
+  const { currentPage } = useEditor((state) => ({
+    currentPage: state.pageOptions.currentPage,
+  }));
+  const rootNodeIdThisPage = currentPage.length > 1 ? `ROOT_${currentPage.slice(1)}` : `ROOT`;
   return (
     <LayerManagerProvider options={options}>
-      <LayerContextProvider id={ROOT_NODE} depth={0} />
+      <LayerContextProvider id={rootNodeIdThisPage} depth={0} />
     </LayerManagerProvider>
   );
 };
